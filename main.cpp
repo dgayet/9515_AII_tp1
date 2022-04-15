0b83bd24208b2ff9194484c213d3f5977c21cb7e
denis <dgayet@fi.uba.ar>
2022-04-15 15:35:23 -0300

    changes to findSubString

--- main.cpp
+++ main.cpp
@@ -21,7 +21,12 @@ int main(int argc, char * argv[]) {
     //std::cout << c << std::endl;
 
     int index = findSubString(ref_string, target_string);
-
+    if (index == -1){
+        std::cout << "The target string has not been found" << std::endl;
+    }
+    else{
+        std::cout << "There is an ocurrence in the index: " << index << std::endl;
+    }
 }
 
 // Preconditions: 
@@ -43,17 +48,33 @@ bool compareString(std::string string1, std::string string2){
     return true;
 }
 
-int finSubString(std::string ref, std::string target){
+int findSubString(std::string ref, std::string target){
     size_t ref_len = ref.length();
     size_t target_len = target.length();
-    size_t i; // for index
+    size_t i, j; // for index
     size_t m = -1; // match index
-    bool match;
+    char ref_ch, target_ch;
 
-    for (i = 0; i < ref_len - target_len; i++){
-        if (ref.at(i) == target.at(0))
+    // three conditions for the for clause 
+
+
+    
+    for (i = 0, j=0; (i < ref_len) && (j < target_len) && (ref_len - m + 1 > target_len); i++){
+        ref_ch = ref.at(i);
+        target_ch = target.at(j);
+        if (std::tolower(ref_ch) == std::tolower(target_ch)){
+            if (j == 0)
+                m = i;
+            j++;
+        }
+        else if (ref.at(i) == target.at(0)){
             m = i;
-            if (i < ref_len - target_len)
-                match = compareString()
+            j = 1;
+        }
     }
+
+    if (j == target_len)
+        return int(m);
+    else
+        return -1;
 }
