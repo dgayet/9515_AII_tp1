d52bc7504621f31a7bfd08087b419daf0e3f2a57
denis <dgayet@fi.uba.ar>
2022-04-18 21:02:51 -0300

    minor changes, added PRE and POS

--- functions.h
+++ functions.h
@@ -1,11 +1,18 @@
 #include <iostream>
 
-bool compareString(std::string string1, std::string string2);
+// PRE: receives an ASCII character 
+// POS: returns the same character in lowercase if it is a non-diacritic letter.
+char formatChar(char ch);
 
-void findSubString(std::string ref, std::string target);
-
+// PRE: receives an array and its lenght (number of elements)
+// POS: prints the elements of the array to the standard output
 void printResults(int * arr, size_t len);
 
+// PRE: receives a dinamyc array of ints and its length, and the value to append to the array
+// POS: returns a copy of the original array with "value" appended to it  
+//      and frees the allocated memory for the original array 
 int * appendValueToArray(int * og_arr, size_t len, int value);
 
-char formatChar(char ch);
+// PRE: receives two strings, ref must be larger than target
+// POS: prints the indexes of the referecene text where ocurrences were found
+void findSubString(std::string ref, std::string target);
--- main.cpp
+++ main.cpp
@@ -1,17 +1,26 @@
+/* ----------- Algoritmos y Programación II - 95.14 ---------
+----------------------- Trabajo Práctico N°1 ----------------
+-------------- Denise Giselle Gayet, padrón: 100828 --------- */
+
 #include <iostream>
 #include "functions.h"
 
 
-// Preconditions: 
-// Postconditions: 
+// PRE: receives 2 arguments from CL, first the reference text, then the target text
+// POST: -
 int main(int argc, char * argv[]) {
     if (argc <3){
         std::cout << "Not enough input arguments" << std::endl;
         return 1;
     }
-   
+    else if(argc > 3){
+        std::cout << "Too many arguments. Will use first two" << std::endl;
+    }
+
     std::string ref_string{argv[1]};
     std::string target_string{argv[2]}; // turn this into a for, for multiple target strings
+
+
    
     findSubString(ref_string, target_string);
     return 0;
